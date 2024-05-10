6600a1e1bcd9c33690283920502b7f8aefadb8f1
Kuba Wlodarczyk <fenris85@gmail.com>
2024-05-10 18:02:39 +0200

    Image fetching

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import { jsPDF } from "jspdf";
 
-function generatePDF() {
-  console.log('GENERATE', );
-  
+function convertTradingViewLink(url) {
+  let identifier = url.split('/').pop();
+  if (identifier == '') {
+    identifier = url.split('/').slice(-2)[0];
+  }
+  const newUrl = `https://s3.tradingview.com/snapshots/i/${identifier}.png`;
+  return newUrl;
+}
+
+function generatePDF(linksContent) {  
   const doc = new jsPDF({
     orientation: 'landscape',
     unit: 'mm',
     format: [237, 150]
 });
 
-  const links = ['https://s3.tradingview.com/snapshots/i/I5qa31yG.png']
+  const tvLinks = linksContent.split('\n');
+
+  const tvImages = tvLinks.map(link => {
+    return convertTradingViewLink(link);
+  });  
 
   let promises = [];
   let images = [];
-  links.forEach(link => {
+  tvImages.forEach(link => {
       promises.push(new Promise((resolve, reject) => {
           let img = new Image();
           img.onload = () => {
@@ -38,11 +51,26 @@ function generatePDF() {
   });
 }
 
-
 function App() {
+
+  const [text, setText] = useState('');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
+
     <div className="App">
-      <button onClick={generatePDF}>Generate PDF</button>
+
+      <h3>TradingView PDF Maker</h3>
+      <p>Paste Tradingview snapshots links (one per line) below and click on Generate PDF</p>
+      <br/>
+      <br/>
+      <textarea onChange={handleTextChange} placeholder="Paste snapshots links here..." id="editor" rows="10" cols="50"></textarea>
+      <br/>
+      <br/>
+      <button onClick={()=>generatePDF(text)}>Generate PDF</button>
     </div>
   );
 }
